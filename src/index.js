d956f3c605357581f6391087ea3ed5364218d194
敖武 <dongdong.fdd@alibaba-inc.com>
2022-02-04 16:32:12 +0800

    chore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require("path")
 
 module.exports = {
-  name: "vuepress-plugin-latest-articles",
+  name: "vuepress-latest-articles",
   plugins: ["last-updated"],
   enhanceAppFiles: [path.resolve(__dirname, "enhanceAppFile.js")],
 }
